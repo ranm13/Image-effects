c9037e9aa9c2985bcf673e68954ae7ce9da95e76
Ran Marciano <ranm13@gmail.com>
2025-04-05 10:31:50 +0300

    Fixed Rust warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console::log_1 as log;
-use base64::{decode, encode};
+use base64::{Engine as _, engine:: general_purpose};
 use image::load_from_memory;
 use image::ImageOutputFormat::Png;
 
 #[wasm_bindgen]
 pub fn grayscale(encoded_file: &str) -> String{
     log(&"Grayscale called".into());
-    let base64_to_vector = decode(encoded_file).unwrap();
+    let base64_to_vector = general_purpose::STANDARD.decode(encoded_file).unwrap();
     log(&"Image decoded".into());
 
     let mut img = load_from_memory(&base64_to_vector).unwrap();
@@ -20,7 +20,7 @@ pub fn grayscale(encoded_file: &str) -> String{
     img .write_to(&mut buffer, Png).unwrap();
     log(&"new image written ".into());
 
-    let encoded_image = encode(&buffer);
+    let encoded_image = general_purpose::STANDARD.encode(&buffer);
     let data_url = format!("data:image/png;base64,{}",encoded_image);
     
     data_url
